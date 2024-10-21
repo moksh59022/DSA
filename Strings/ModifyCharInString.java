41cda410ff75b25a323cebdb750f835f5f1f9deb
moksh59022 <vsmokshsharma688@gmail.com>
2024-10-22 00:26:27 +0530

    Delete Strings/ModifyCharInString.java

--- Strings/ModifyCharInString.java
+++
@@ -1,9 +0,0 @@
-package Strings;
-
-public class ModifyCharInString {
-    public static void main(String[] args) {
-        String s = "Moksh";
-        s = s.substring(0,2)+'Y'+s.substring(3);
-        System.out.println(s);
-    }
-}
