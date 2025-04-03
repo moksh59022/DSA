a6febf6828766a0237c5359ff0a87cfb87e726c3
moksh59022 <vsmokshsharma688@gmail.com>
2025-04-03 21:56:36 +0530

    working

--- Brown/Leetcode78Recursion.java
+++ Brown/Leetcode78Recursion.java
@@ -22,27 +22,20 @@ public class Leetcode78Recursion {
         }
     }
 
-    static List<List<Integer>> arr;
-    public static void helper(int i,int[] nums,ArrayList<Integer> ans){
-        if(i==nums.length){
-            ArrayList<Integer> list = new ArrayList<>();
-            for(int j = 0;j<ans.size();j++){
-                list.add(ans.get(j));
-            }
-            arr.add(list);
+    public static void helper(int idx,int[] nums,List<List<Integer>> ans , List<Integer> ds){
+        if(idx==nums.length){
+            ans.add(new ArrayList<>(ds));
             return;
         }
-        helper(i+1,nums,ans); // not take
-        ans.add(nums[i]);
-        helper(i+1,nums,ans); // take
-        ans.remove(ans.size()-1);
+        helper(idx+1,nums,ans,ds); // not take
+        ds.add(nums[idx]);
+        helper(idx+1,nums,ans,ds); // take
+        ds.remove(ds.size()-1);
     }
     public static List<List<Integer>> subsets(int[] nums) {
-        arr = new ArrayList<>();
-        ArrayList<Integer> ans = new ArrayList<>();
-        helper(0,nums,ans);
-
-        arr.sort((a,b) -> {
+        List<List<Integer>> ans = new ArrayList<>();
+        helper(0,nums,ans,new ArrayList<>());
+        ans.sort((a,b) -> {
             for (int i = 0; i < Math.min(a.size(), b.size()); i++) {
                 if(!a.get(i).equals(b.get(i))){
                     return a.get(i) - b.get(i);
@@ -50,7 +43,7 @@ public class Leetcode78Recursion {
             }
             return  a.size() - b.size();
         });
-        return arr;
+        return ans;
     }
     
 }
